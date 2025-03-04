e84a557fbc87b6fe72cd6737adaf91a4e39fe041
Aditya Karki <adityakarki201@gmail.com>
2025-03-04 22:10:43 +0530

    Remove all files and start fresh

--- AI-Apis/src/routes/v1/index.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import userRoutes from "../userRoutes";
-const appRouterV1 = express.Router();
-
-appRouterV1.use("/users", userRoutes);
-
-export default appRouterV1;
