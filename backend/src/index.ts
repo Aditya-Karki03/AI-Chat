4278e80453c1a0af0aca2c9306f5198ef914d000
Aditya Karki <adityakarki201@gmail.com>
2025-03-05 22:28:57 +0530

    Add dependencies

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import { createServer } from "node:http";
+const app = express();
+const server = createServer(app);
+
+const port = 3001;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
